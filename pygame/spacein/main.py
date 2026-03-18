d58aa5d09bd481d79e50802207dae3a393f3e9e1
MM <margarita02012009@gmail.com>
2026-03-18 14:04:26 +0100

    add

---
+++ pygame/spacein/main.py
@@ -0,0 +1,32 @@
+import pygame
+from settings import *
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Space Invaders")
+running = True
+
+def vypis_menu():
+    screen.blit(title_text, title_rect)
+    screen.blit(play_text, play_rect)
+    screen.blit(settings_text, settings_rect)
+    screen.blit(quit_text, quit_rect)
+    
+state = "MENU"
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = event.pos
+        if state == "MENU":
+           if play_rect.collidepoint(mouse_pos):
+               state = "PLAYING"
+            elif settings_rect.collidepoint(mouse_pos):
+                state ="SETTINGS"
+            elif quit_rect.collidepoint(mouse_pos):
+               running = False
+    if state == "MENU":
+        screen.fill((0, 0, 0))
+        vypis_menu()
--- pygame/uhybaní/main.py
+++ pygame/uhybaní/main.py
@@ -18,7 +18,7 @@ block = Block()
 b_group = pygame.sprite.Group()
 b_group.add(block)
 
-b_spawn = pygame.USEREVENT + 1
+B_SPAWN = pygame.USEREVENT + 1
 pygame.time.set_timer(b_spawn,1500)
 
 score = 0
@@ -26,16 +26,30 @@ score_font = pygame.font.SysFont("Arial", 24)
 score_text = score_font.render(f"Score:{score}", True, (255, 255, 255))
 score_rect = score_text.get_rect(topleft=(10,10))
 
+
+#try:
+   # with open("hightscore.txt", "w") as soubor:
+        #hightscore = int(soubor.read().strip())
+
+
+
 while running:
     screen.fill((25,25,112))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == b_spawn:
+        if event.type == B_SPAWN:
             b_group.add(Block())
             score += 1
             score_text = score_font.render(f"Score:{score}", True,(255, 255, 255))
+        if score % 2 == 0 and score != 0:
+            spawning_timer = max(500, spawning_timer - 100)
+            pygame.time.set_timer(B_SPAWN, spawning_timer)
+        
+
+
             screen.blit(score_text, score_rect)
+
     
     hrac_group.update()
     hrac_group.draw(screen)
@@ -45,9 +59,15 @@ while running:
     if pygame.sprite.spritecollide(hrac, b_group, True, pygame.sprite.collide_mask): #колизия работает при столеновении
         pygame.time.delay(500)
         print("KOLIZE!!!!!!!!!!!!!")
+        if score >= hightscore:
+            hightscore = score
         with open("hightscore.txt", "w") as soubor:
-            soubor.write(f"{score}\n")
-        running= False
+            soubor.write(f"{hightscore}\n")
+        #with open("hightscore.txt", "r") as soubor:
+
+
+
+        running = False
 
 
 
